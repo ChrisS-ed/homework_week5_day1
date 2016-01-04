74b8bcb9cfbfb6a21a21714709638a21abc88a3c
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-04 15:32:08 +0000

    added quantities controller

---
+++ app/controllers/quantities_controller.rb
@@ -0,0 +1,11 @@
+class QuantitiesController < ApplicationController
+    
+  def index
+    @quantities = Quantity.all
+  end
+
+  def show
+    @quantity = Quantity.find(params[:id])
+  end
+  
+end
